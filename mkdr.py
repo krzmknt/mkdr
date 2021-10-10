3984b1aed1cabd847b8fd5e9ecba865d6686cddf
krzmknt <krzmknt@gmail.com>
2021-10-11 00:52:15 +0900

    Code Inspection
    - fix some variable names
    - fix some line breaks

--- mkdr.py
+++ mkdr.py
@@ -1,42 +1,72 @@
+# --------------------------------------------------------------------------- #
+# Package
+# --------------------------------------------------------------------------- #
 import yaml
 import pathlib
 import os
 import argparse
 import shutil
 import sys
+import termios
 from collections import namedtuple
 
 
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 # Argument
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--force', action='store_true', help='Overwrite the existing objects')
-parser.add_argument('-n', '--name', type=str, help='Specify the filename')
-parser.add_argument('-r', '--remove', action='store_true', help='Remove mode')
-parser.add_argument('-o', '--reorg', action='store_true', help="Not organize new objects, but rearrange the existing objects.")
-parser.add_argument('-s', '--save', action='store_true', help="Save mode")
+parser.add_argument('-f', '--force',
+    action='store_true',
+    help='Execute commands without confirmation.')
+
+parser.add_argument('-n', '--name',
+    type=str,
+    help='Specify the configuration file name.')
+
+parser.add_argument('-r', '--remove',
+    action='store_true',
+    help='Execute mkdr in the remove mode.')
+
+parser.add_argument('-o', '--reorg',
+    action='store_true',
+    help="Execute mkdr in the reorg mode.")
+
+parser.add_argument('-s', '--save',
+    action='store_true',
+    help="Execute mkdr in the save mode.")
+
 arg = parser.parse_args()
 
 
-# ---------------------------------------------------------------------------- #
-# Variables
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
+# Constant
+# --------------------------------------------------------------------------- #
 INDENT = 2
-Path = namedtuple('Path', ['type','path'])
-sign_dir = 'd'
-sign_file = 'f'
+SIGD = 'd'
+SIGF = 'f'
+
+
+# --------------------------------------------------------------------------- #
+# Color Text
+# --------------------------------------------------------------------------- #
 red = lambda str: print('\033[31m'+str+'\033[0m')
 green =  lambda str: print('\033[32m'+str+'\033[0m')
 
 
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
+# Type
+# --------------------------------------------------------------------------- #
+Ob = namedtuple('Ob', ['type','path'])
+
+
+# --------------------------------------------------------------------------- #
 # Check
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 def check():
     checkUser()
     checkMode()
     checkOptionCombination()
+
 
 def checkUser():
     if os.getuid() == 0:
@@ -45,8 +75,8 @@ def checkUser():
 
 
 def checkMode():
-    modes = arg.remove + arg.reorg + arg.save
-    if modes > 1:
+    numOfModes = arg.remove + arg.reorg + arg.save
+    if numOfModes > 1:
         red('ModeError: Only one mode can be specified.')
         sys.exit()
 
@@ -58,49 +88,45 @@ def checkOptionCombination():
             sys.exit()
 
 
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 # Module
-# ---------------------------------------------------------------------------- #
-def getPathsFromOrgYaml(filename):
+# --------------------------------------------------------------------------- #
+def getObsFromYml(filename):
     try:
         with open(filename) as f:
-            paths_yml = yaml.load(f, Loader=yaml.SafeLoader)
+            yml = yaml.load(f, Loader=yaml.SafeLoader)
     except FileNotFoundError as err:
         red('FileNotFoundError: No such file: \'{}\''.format(filename))
         sys.exit()
 
-    isDirectory = lambda d: isinstance(d,dict)
+    isDir = lambda ob: isinstance(ob,dict)
     getDirName = lambda d: list(d.keys())[0]
     getDirContents = lambda d: d[getDirName(d)]
 
-    path = ['.']
-    paths = []
-    def recursiveMkdir(paths_yml, path):
-        for d in paths_yml:
-            if isDirectory(d):
-                path.append(getDirName(d))
-                paths.append(Path(sign_dir, '/'.join(path)))
-                recursiveMkdir(getDirContents(d), path)
-            elif d is not None:
-                paths.append(Path(sign_file, '/'.join(path)+'/'+d))
-        path.pop()
-
-    recursiveMkdir(paths_yml, path)
-    return paths
+    def getObsRcsvly(yml, basePath):
+        obs = []
+        for ob in yml:
+            if isDir(ob):
+                dirName = getDirName(ob)
+                obs.append(Ob(SIGD, os.path.join(*basePath,dirName)))
+                obs += getObsRcsvly(getDirContents(ob), basePath+[dirName])
+            elif ob is not None:
+                obs.append(Ob(SIGF, os.path.join(*basePath,ob)))
+        return obs
+    return getObsRcsvly(yml, ['.'])
 
 
-def checkNotExist(paths):
-    paths_exist = [p.path for p in paths if os.path.exists(p.path)]
-    if paths_exist:
-        red('CannotOverwriteError: The object(s) already exist(s). If you overwrite, please specify `--force` option.')
-        for p in paths_exist:
-            red('  '+p)
+def checkNotExist(obs):
+    obsExist = [ob.path for ob in obs if os.path.exists(ob.path)]
+    if obsExist:
+        red('CannotOverwriteError: The object(s) already exist(s). '\
+            'If you overwrite, please specify `--force` option.')
+        for ob in obsExist:
+            red('  '+ob)
         sys.exit()
 
 
 def getInput():
-    import sys
-    import termios
     fd = sys.stdin.fileno()
     old = termios.tcgetattr(fd)
     new = termios.tcgetattr(fd)
@@ -114,65 +140,71 @@ def getInput():
     return ch
 
 
-deleteObjects = lambda paths: [shutil.rmtree(p.path) for p in paths if os.path.exists(p.path)]
+delObs = lambda obs: [shutil.rmtree(ob.path) for ob in obs \
+    if os.path.exists(ob.path)]
 
 
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 # Mode
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 def make(filename):
-    paths = getPathsFromOrgYaml(filename)
+    obs = getObsFromYml(filename)
     if arg.force:
-        deleteObjects(paths)
+        delObs(obs)
     else:
-        checkNotExist(paths)
+        checkNotExist(obs)
 
-    green('Success: %d directories/files have been created!'%len(paths))
-    makeObject = lambda p: os.makedirs(p.path) if p.type==sign_dir else pathlib.Path(p.path).touch()
-    for p in paths:
-        green((' '*2)+p.path)
-        makeObject(p)
-
-
-def remove(filename):
-    paths = getPathsFromOrgYaml(filename)
-    if arg.force:
-        red('OptionError: The option \'--force\' cannot be specified in the remove mode.')
-        sys.exit()
-    else:
-        deleteObjects(paths)
-    green('Success: the directories/files have been removed!')
-
-
-def reorg(filename):
-    print('Future feature: The reorganizing will be implemented in the future release.')
+    green('Success: %d directories/files have been created!' % len(obs))
+    makeOb = lambda ob: os.makedirs(ob.path) if ob.type == SIGD else \
+        pathlib.Path(ob.path).touch()
+    for ob in obs:
+        green((' '*2)+ob.path)
+        makeOb(ob)
 
 
 def save(filename):
     def dirsToYaml(path):
-        from os import listdir as ld
-        from os.path import join
-        from os.path import isfile
-        import yaml
-        def getObsAtCurrentDir(basePath,dirName):
-            nbp = basePath + [dirName]
-            obsAtCurrentDir = [ob if isfile(join(*nbp,ob)) else {ob:getObsAtCurrentDir(nbp,ob)} for ob in ld(join(*nbp))]
-            return obsAtCurrentDir if len(obsAtCurrentDir) > 0 else [None]
-        return getObsAtCurrentDir([],path)
+
+        def getObsInCD(basePath,dirName):
+            nbp = basePath + [dirName] # nextBasePath
+            join = os.path.join
+            isf = os.path.isfile
+            ld = os.listdir
+            obsInCD = [obn if isf(join(*nbp,obn)) else \
+                {obn:getObsInCD(nbp,obn)} for obn in ld(join(*nbp))]
+            return obsInCD if len(obsInCD) > 0 else [None]
+
+        return getObsInCD([],path)
 
     if os.path.exists(filename) and not arg.force:
-        print('The file \'{}\' already exists. Do you overwrite with this saving file? [Y/n]:'.format(filename))
+        print('The file \'{}\' already exists. '\
+            'Do you overwrite with this saving file? [Y/n]:'.format(filename))
         if getInput() != 'Y':
             sys.exit()
+
 
     with open(filename, 'w') as file:
         yaml.dump(dirsToYaml('.'), file)
     green('Successfully Saved!')
 
 
-# ---------------------------------------------------------------------------- #
+def remove(filename):
+    if not arg.force:
+        print('Do you remove the objects? [Y/n]:'.format(filename))
+        if getInput() != 'Y':
+            sys.exit()
+    delObs(getObsFromYml(filename))
+    green('Success: the objects have been removed!')
+
+
+def reorg(filename):
+    print('Future feature: The reorganizing will be implemented in the '\
+        'future release.')
+
+
+# --------------------------------------------------------------------------- #
 # Entrypoint
-# ---------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
 def main():
     check()
     filename = 'mkdr-config.yml' if arg.name is None else arg.name
